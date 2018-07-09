f49411c2934a8c15576f042fc72479fa2c74f90a
Shakeria Smith <shakerias2@gmail.com>
2018-07-09 18:50:29 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,2 +1,7 @@
 def using_until
   #puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10
+  counter = 0
+  until counter == 20
+    puts "Wingardium Leviosa"
+    counter += 1
+  end
